42fd12e21b76f8333f62d7d2b1a39b0064eebf16
Arjun <letsmailarjunmurali@gmail.com>
2022-10-05 12:36:25 +0530

    react_JSX

--- RoadToReact/hacker-stories/src/App.js
+++ RoadToReact/hacker-stories/src/App.js
@@ -1,15 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const name = "React"; // variables defined outside function
+const name = "React";
+const obj = { firstname: "Arjun", lastname: "Murali" };
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
-  const title = "Zoamto"; // varible defined inside function
+  const title = "Zoamto";
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Hello {getTitle("World!!!!!")}</h1>
+      <h2>{obj.firstname}</h2>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
     </div>
   );
 }
 
 export default App;
+
+/*
+
+JSX - combines HTML and JavaScript. 
+JSX-specific attributes like className and onClick instead of class and onclick, which follows the camel case
+*/
