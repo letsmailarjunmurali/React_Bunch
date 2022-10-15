dcac52d5edc358c696b9b60459864bbdd2cff671
Arjun <letsmailarjunmurali@gmail.com>
2022-10-15 13:06:43 +0530

    inline_handler_in_JSX

--- RoadToReact/hacker-stories/readme.txt
+++ RoadToReact/hacker-stories/readme.txt
@@ -1,7 +1,7 @@
-
-All the essential steps are marked with comments that are explained step by step:
-• (A) First, create a ref with React’s useRef Hook. This ref object is a persistent value which stays intact over the lifetime of a React component. It comes with a property called current, which, in contrast to the ref object, can be changed.
-• (B) Second, the ref is passed to the input field’s JSX-reserved ref attribute and the element instance is assigned to the changeable current property.
-• (C) Third, opt into React’s lifecycle with React’s useEffect Hook, performing the focus on the input field when the component renders (or its dependencies change).
-• (D) And fourth, since the ref is passed to the input field’s ref attribute, its current property gives access to the element. Execute its focus programmatically as a side-effect, but only if isFocused is set and the current property is existent.
-This was an example of how to move from declarative to imperative programming in React. It’s not always possible to go the declarative way, so the imperative approach can be performed whenever it’s necessary.
+we have made the list of stories stateful with React’s useState Hook; passed the still searched stories down as props to the List component, 
+and implemented a callback handler (handleRemoveStory) and handler (handleRemoveItem) to be used in their respective components to remove a story by clicking on a button. 
+In order to implement this feature, we applied many lessons learned from before: state, props, handlers, and callback handlers.
+You may have noticed that we had to introduce an additional handleRemoveItem handler in the Item component which is in charge to execute the incoming onRemoveItem callback handler. 
+If you want to make this more elegant, you can use an inline handler which would allow you to execute the callback handler function in the Item component right in the JSX. 
+There are two solutions using the incoming onRemoveItem function in the Item component as an inline handler. First, using JavaScript’s bind method:
+Using JavaScript’s bind method110 on a function allows us to bind arguments directly to that function that should be used when executing it. The bind method returns a new function with the bound argument attached.
--- RoadToReact/hacker-stories/src/App.js
+++ RoadToReact/hacker-stories/src/App.js
@@ -17,29 +17,35 @@ const useSemiPersistentState = (key, initialState) => {
   }, [value, key]);
   return [value, setValue];
 };
-
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 const App = () => {
   console.log("App renders");
 
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  const [stories, setStories] = React.useState(initialStories);
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "");
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -56,7 +62,7 @@ const App = () => {
       <h2>{obj.firstname}</h2>
       {/* B */}
       <Search search={searchTerm} onSearch={handleSearch} />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
@@ -124,27 +130,40 @@ const InputWithLabel = ({
   );
 };
 
-const List = ({ list }) => {
+const List = ({ list, onRemoveItem }) => {
   console.log("List renders");
 
   return (
     <ul>
-      {list.map(({ objectID, ...item }) => (
-        <Item key={objectID} {...item} />
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
       ))}
     </ul>
   );
 };
 
-const Item = ({ title, url, author, num_comments, points }) => (
-  <li>
-    <span>
-      <a href={url}>{title}</a>
-    </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
-  </li>
-);
+const Item = ({ item, onRemoveItem }) => {
+  // const handleRemoveItem = () => {
+  //   onRemoveItem(item);
+  // };
+
+  return (
+    <>
+      <li>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <button type="button" onClick={onRemoveItem.bind(null, item)}>
+            Dismiss
+          </button>
+        </span>
+      </li>
+    </>
+  );
+};
 
 export default App;
