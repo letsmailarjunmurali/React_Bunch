dc05636e9635dc59d77b0dbe8090f132c5aef8fb
Arjun <letsmailarjunmurali@gmail.com>
2022-11-14 13:47:44 +0530

    explicit_data-fetching

--- RoadToReact/hacker-stories/readme.txt
+++
@@ -1,5 +0,0 @@
-All state management in this application makes heavy use of React’s useState Hook. On the other hand, React’s useReducer Hook gives you more sophisticated state management.
-We’ll move the stories state management from the useState hook to a new useReducer hook. First, introduce a reducer function outside of your components.
-A reducer function always receives state and action. Based on these two arguments, a reducer always returns a new state:
-In the App component, exchange useState for useReducer for managing the stories.
-The new hook receives a reducer function and an initial state as arguments and returns an array with two items. The first item is the current state; the second item is the state updater function (also called dispatch function):
--- RoadToReact/hacker-stories/src/App.js
+++ RoadToReact/hacker-stories/src/App.js
@@ -54,11 +54,13 @@ const App = () => {
   });
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "");
 
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
   const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -67,7 +69,7 @@ const App = () => {
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
-  }, [searchTerm]);
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
@@ -77,8 +79,12 @@ const App = () => {
     dispatchStories({ type: "REMOVE_STORY", payload: item });
   };
 
-  const handleSearch = (event) => {
+  const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
   const searchedStories = stories.data.filter(function (story) {
@@ -90,7 +96,10 @@ const App = () => {
       <h1>Hello {getTitle("World!!!!!")}</h1>
       <h2>{obj.firstname}</h2>
       {/* B */}
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearchInput} />
+      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
+        Submit
+      </button>
       {stories.isError && <p>Something went wrong</p>}
       {stories.isLoading ? (
         <p>Loading......</p>
